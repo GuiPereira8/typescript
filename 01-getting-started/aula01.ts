496df9b49e1fa446982caa969662854292584984
Guilherme Pereira <guiipereiraarts@gmail.com>
2026-04-29 15:40:31 -0300

    Configurando o ambiente

--- 01-getting-started/aula01.ts
+++ 01-getting-started/aula01.ts
@@ -3,3 +3,12 @@ const nome = "Guilherme";
 
 console.log(`Bem vindo, ${nome}`);
 console.log(message);
+
+class Sistema {
+  ip: string;
+
+  constructor(ip: string) {
+    this.ip = ip;
+  }
+}
+const servidor1 = new Sistema("192.168.500.2");
