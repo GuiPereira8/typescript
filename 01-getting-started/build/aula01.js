09c1da1781767c9be0e56abe073173b4c79d0de9
Guilherme Pereira <guiipereiraarts@gmail.com>
2026-04-30 10:23:59 -0300

    Arrumando pastas dos arquivos

--- 01-getting-started/build/aula01.js
+++ 01-getting-started/build/aula01.js
@@ -1,14 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 const message = "Olá, Mundo!";
 const nome = "Guilherme";
-
 console.log(`Bem vindo, ${nome}`);
 console.log(message);
-
 class Sistema {
-  ip: string;
-
-  constructor(ip: string) {
-    this.ip = ip;
-  }
+    ip;
+    constructor(ip) {
+        this.ip = ip;
+    }
 }
 const servidor1 = new Sistema("192.168.500.2");
---
+++ 01-getting-started/src/aula01.ts
@@ -0,0 +1,5 @@
+const message = "Olá, Mundo!";
+const nome = "Guilherme";
+
+console.log(`Bem vindo, ${nome}`);
+console.log(message);
