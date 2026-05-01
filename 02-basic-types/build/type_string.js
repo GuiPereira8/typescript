745cb8a745928e9af96a64b391ad9bb037a91923
Guilherme Pereira <guiipereiraarts@gmail.com>
2026-05-01 15:23:41 -0300

    Variável Tipo String TypeScript

---
+++ 02-basic-types/build/type_string.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let linguagem;
+linguagem = "JavaScript";
+// linguagem = 2026; A variável foi definida como String e não pode ser alterada para outro tipo.
+console.log(linguagem);
---
+++ 02-basic-types/src/type_string.ts
@@ -0,0 +1,5 @@
+let linguagem: string;
+
+linguagem = "JavaScript";
+// linguagem = 2026; A variável foi definida como String e não pode ser alterada para outro tipo.
+console.log(linguagem);
