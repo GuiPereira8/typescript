01b7b38e41eaafbc381a6994e27732270f5430e0
Guilherme Pereira <guiipereiraarts@gmail.com>
2026-05-01 15:14:48 -0300

    Entendendo os Tipos Básicos do TypeScript

---
+++ 02-basic-types/build/type_inference.js
@@ -0,0 +1,9 @@
+"use strict";
+//Tipagem DINAMICA
+Object.defineProperty(exports, "__esModule", { value: true });
+let curso = "TypeScript";
+let tecnologias = ["C#", "Java", "React JS", "Ruby", 310115];
+tecnologias.push("JavaScript");
+tecnologias.push(2026);
+console.log(curso);
+console.log(tecnologias);
---
+++ 02-basic-types/src/type_inference.ts
@@ -0,0 +1,11 @@
+//Tipagem DINAMICA
+
+let curso = "TypeScript";
+
+let tecnologias = ["C#", "Java", "React JS", "Ruby", 310115];
+
+tecnologias.push("JavaScript");
+tecnologias.push(2026);
+
+console.log(curso);
+console.log(tecnologias);
