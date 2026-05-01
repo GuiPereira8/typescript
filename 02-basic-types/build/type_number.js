6dd2e2cf6efa76b3b3af1306dadd8be1b4960143
Guilherme Pereira <guiipereiraarts@gmail.com>
2026-05-01 15:31:02 -0300

    Tipo number

---
+++ 02-basic-types/build/type_number.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// Type number = (int, float, hex, binary)
+let primeiroValor = 32;
+primeiroValor = 32.9;
+let segundoValor;
+segundoValor = 19.4;
+console.log(primeiroValor + segundoValor);
---
+++ 02-basic-types/src/type_number.ts
@@ -0,0 +1,9 @@
+// Type number = (int, float, hex, binary)
+let primeiroValor: number = 32;
+
+primeiroValor = 32.9;
+
+let segundoValor: number;
+segundoValor = 19.4;
+
+console.log(primeiroValor + segundoValor);
