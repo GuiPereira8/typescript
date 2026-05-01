934b2dcc2dd054d6fac096bafddd25255d2f4d2d
Guilherme Pereira <guiipereiraarts@gmail.com>
2026-05-01 14:28:42 -0300

    Update aula01.js

--- 01-getting-started/build/aula01.js
+++ 01-getting-started/build/aula01.js
@@ -4,10 +4,3 @@ const message = "Olá, Mundo!";
 const nome = "Guilherme";
 console.log(`Bem vindo, ${nome}`);
 console.log(message);
-class Sistema {
-    ip;
-    constructor(ip) {
-        this.ip = ip;
-    }
-}
-const servidor1 = new Sistema("192.168.500.2");
