e099335eaa31fffbb2617353891fc6d0d46342b6
Guilherme Pereira <guiipereiraarts@gmail.com>
2026-05-06 09:55:01 -0300

    Union Type in TypeScript

---
+++ 02-basic-types/build/union_type.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let userId;
+// userId = 192;
+userId = "192";
+console.log(typeof userId);
---
+++ 02-basic-types/src/union_type.ts
@@ -0,0 +1,8 @@
+/*Union type = Dizer a variável que ela pode guardar os tipos que foram
+definidos na variável, no exemplo abaixo apenas numeros e strings. 
+*/
+let userId: number | string;
+
+// userId = 192;
+userId = "192";
+console.log(typeof userId);
