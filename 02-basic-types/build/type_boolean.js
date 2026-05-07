b1532de762b1f8c0903fba3562b6f4cac8aa7f24
Guilherme Pereira <guiipereiraarts@gmail.com>
2026-05-07 09:34:06 -0300

    Type boolean in TypeScript

---
+++ 02-basic-types/build/type_boolean.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let estaAutenticado = true;
+let codeStatus = "";
+estaAutenticado = Boolean(codeStatus);
+console.log(estaAutenticado);
---
+++ 02-basic-types/src/type_boolean.ts
@@ -0,0 +1,10 @@
+//Type boolean
+let estaAutenticado: boolean = true;
+
+// estaAutenticado = false;
+
+let codeStatus: string = "";
+
+//Tudo que for diferente de zero, string vazia, undefined, será verdadeiro (True).
+estaAutenticado = Boolean(codeStatus);
+console.log(estaAutenticado);
