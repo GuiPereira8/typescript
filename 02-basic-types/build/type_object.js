08cbc56ab778da7fef73919b8af6e66add550199
Guilherme Pereira <guiipereiraarts@gmail.com>
2026-05-08 14:25:26 -0300

    Tipo Objeto em TypeScript

---
+++ 02-basic-types/build/type_object.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let novoUsuario;
+novoUsuario = {
+    nome: "Guilherme",
+    email: "exemplo@gmail.com",
+    id: 1,
+};
+console.log(novoUsuario);
---
+++ 02-basic-types/src/type_object.ts
@@ -0,0 +1,8 @@
+//Type object
+let novoUsuario: object;
+novoUsuario = {
+  nome: "Guilherme",
+  email: "exemplo@gmail.com",
+  id: 1,
+};
+console.log(novoUsuario);
