e534c830920d19661ef386215abb0f6ad82725e6
Guilherme Pereira <guiipereiraarts@gmail.com>
2026-05-11 14:44:46 -0300

    Type null e undefined em TypeScript

---
+++ 02-basic-types/build/type_null.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let nome;
+nome = "Olá, Mundo!";
+console.log(nome);
+let userName;
+userName = 2026;
+console.log(userName);
---
+++ 02-basic-types/src/type_null.ts
@@ -0,0 +1,8 @@
+// Type Null e Undefined
+let nome: null | string;
+nome = "Olá, Mundo!";
+console.log(nome);
+
+let userName: undefined | number;
+userName = 2026;
+console.log(userName);
